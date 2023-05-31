403a6318e267b1162cc5707a00593515e3826ce8
ro23012 <r.oikw0829akd3131@icloud.com>
2023-05-31 17:04:55 +0900

    家でやる

---
+++ baseballGame/Gamechar.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseballGame
+{
+    internal class Gamechar
+    {
+        protected string name;
+        protected int speed;
+        protected int power;
+
+        public Gamechar(string name, int speed, int power)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.power = power;
+        }
+
+
+        public void Show()
+        {
+            Console.WriteLine("選手名：{0}　球速：{1}　パワー；{3}",this.name,this.speed,this,power);
+        }
+
+        public void Throw()
+        {
+
+
+            //ストライク　ボール　アウト　の変数作成
+            string [] strike = {"●", "●●", "●●●" };
+            string [] boll = { "●", "●●", "●●●", "●●●●" };
+
+            //乱数の作成
+            Random ran = new Random();
+
+            //カウントするやつ
+            int countS = 0;
+            int countB = 0;
+
+
+            string select = Console.ReadLine();
+
+            if (select == "T")
+            {
+                while (strike.Length > 3 || boll.Length > 4)
+                {
+                    int random = ran.Next(1, 5);
+                    int exeSpeed = random * this.Speed / 100;
+                    if (exeSpeed > 5)
+                    {
+                        countS = countS + 1;
+                        Console.WriteLine(strike[countS]);
+
+                    }
+                    else if (exeSpeed < 5)
+                    {
+                        countB = countB + 1;
+                        Console.WriteLine(boll[countB]);
+                    }
+                }
+
+            }
+
+
+        }
+
+
+        public string Name
+        {
+            set { name = value; }
+            get { return this.name; }
+        }
+
+        public int Speed
+        {
+            set { this.speed = value; }
+            get { return this.speed; }
+        }
+
+        public int Power
+        {
+            set { this.power = value; }
+            get { return this.power; }
+        }
+
+
+
+
+    }
+
+
+
+}
--- baseballGame/Program.cs
+++ baseballGame/Program.cs
@@ -10,6 +10,55 @@ namespace baseballGame
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("パワプロさん");
+
+            //選手選択
+            Console.WriteLine("選手を選択してください");
+            Console.WriteLine("A,　大谷さん");
+            Console.WriteLine("B,　佐々木さん");
+            Console.WriteLine("C, 及川さん");
+
+
+
+
+            string select = Console.ReadLine();
+            if(select == "A")
+            {
+                Gamechar otani = new Gamechar("大谷", 170, 100);
+                otani.Show();
+                Console.WriteLine("T ボールを投げる");
+                otani.Throw();
+                
+            }
+            else if (select == "B")
+            {
+                Gamechar sasaki = new Gamechar("佐々木", 160, 50);
+                sasaki.Show();
+            }
+            else if (select == "C")
+            {
+                Gamechar oikawa = new Gamechar("及川", 50, 15);
+                oikawa.Show();
+            }
+            else
+            {
+                
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
